82768009c7adb305b0e5fa6a09c3c973ec463134
yohan <pyh52894@gmail.com>
2021-03-24 21:53:09 +0900

    21.03.24 KH study

--- 01_helloweb/src/com/kh/web/servlet/TestPerson2Servlet.java
+++ 01_helloweb/src/com/kh/web/servlet/TestPerson2Servlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -78,23 +79,58 @@ public class TestPerson2Servlet extends HttpServlet {
 		System.out.println("foodArr = " + Arrays.toString(foodArr));
 
 		// 2. 업무로직
+		String recommendation = "";
+		String colorName = "";
+		switch (color) {
+		case "빨강":
+			recommendation = "빨간 짬뽕";
+			colorName = "red";
+			break;
+		case "노랑":
+			recommendation = "노란 참외";
+			colorName = "yellow";
+			break;
+		case "초록":
+			recommendation = "초록 시금치";
+			colorName = "green";
+			break;
+		case "파랑":
+			recommendation = "파랑 파워에이드";
+			colorName = "blue";
+			break;
+		}
+
+		// jsp에 새로 생선된 data를 전달하기 위해 request에 속성으로 저장한다.
+		// key:String, value:Object 저장
+		request.setAttribute("recommendation", recommendation);
+		request.setAttribute("colorName", colorName);
+
+		// 3. html작성을 jsp에 위임.
+		// /(슬레시)로 시작한다면, /WebContent에서 조회함.
+		RequestDispatcher reqDispatcher = request.getRequestDispatcher("/servlet/testPersonEnd.jsp");
+		reqDispatcher.forward(request, response);
 
 		// 3. 응답메세지처리 html
-		response.setContentType("text/html; charset=utf-8");
-		PrintWriter out = response.getWriter();
-		out.println("<!DOCTYPE html>");
-		out.println("<html>");
-		out.println("<head>");
-		out.println("<title>개취 검사 결과</title>");
-		out.println("</head>");
-		out.println("<body>");
-		out.println("<h1>개인 취향 검사 결과 GET</h1>");
-		out.println("<p>" + name + "님의 개인취향 검사 결과는 </p>");
-		out.println("<p>" + color + "색을 좋아합니다. </p>");
-		out.println("<p>좋아하는 동물은 " + animal + "입니다.</p>");
-		out.println("<p>좋아하는 음식은 " + Arrays.toString(foodArr) + "입니다. </p>");
-		out.println("</body>");
-		out.println("</html>");
+//		response.setContentType("text/html; charset=utf-8");
+//		PrintWriter out = response.getWriter();
+//		out.println("<!DOCTYPE html>");
+//		out.println("<html>");
+//		out.println("<head>");
+//		out.println("<title>개취 검사 결과</title>");		
+//		out.println("<style>");
+//		out.println(".recommendation { font-size: 2em; color: lime; text-decoration: underline; }");
+//		out.println("</style>");
+//		out.println("</head>");
+//		out.println("<body>");
+//		out.println("<h1>개인 취향 검사 결과 jsp</h1>");
+//		out.println("<p>" + name + "님의 개인취향 검사 결과는 </p>");
+//		out.println("<p>" + color + "색을 좋아합니다. </p>");
+//		out.println("<p>좋아하는 동물은 " + animal + "입니다.</p>");
+//		out.println("<p>좋아하는 음식은 " + Arrays.toString(foodArr)+ "입니다. </p>");
+//		out.println("<hr>");
+//		out.println("<p class='recommendation'>오늘은 " + recommendation + " 어떠세요?</p>");
+//		out.println("</body>");
+//		out.println("</html>");
 
 	}
 
